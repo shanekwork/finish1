e9fa9dd491f9081e36f1023684ce7752cf6ddb24
Shane Kelly <shanekweb@icloud.com>
2015-07-17 15:53:58 +0100

    check stuff

---
+++ app/controllers/spree/base_controller.rb
@@ -0,0 +1,25 @@
+require 'cancan'
+require_dependency 'spree/core/controller_helpers/strong_parameters'
+
+class Spree::BaseController < ApplicationController
+
+	before_filter :check_stuff
+
+
+    def check_stuff
+      @user = Spree::User.first
+      company = Company.where(user_id: 1)
+      @c = @user.company 
+    end
+    
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::RespondWith
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Search
+  include Spree::Core::ControllerHelpers::Store
+  include Spree::Core::ControllerHelpers::StrongParameters
+
+  respond_to :html
+end
+
+require 'spree/i18n/initializer'
