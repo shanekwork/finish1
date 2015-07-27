9d00d8169ac932d0ad39ef17b8837fa4585a94c8
Shane Kelly <shanekweb@icloud.com>
2015-07-27 13:19:55 +0100

    mig email mailer protected attr controller new edit company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707143203) do
+ActiveRecord::Schema.define(version: 20150727121444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20150707143203) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "email"
+    t.string   "mailer"
   end
 
   create_table "import_controls", force: true do |t|
